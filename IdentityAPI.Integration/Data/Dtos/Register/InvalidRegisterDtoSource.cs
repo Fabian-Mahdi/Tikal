367f904b4487db8eea9e9bc57c6bfa5214e86171
Fabian Mahdi <fabian@mahdi.at>
2025-06-30 10:33:21 +0200

    added integration tests for register endpoint

---
+++ IdentityAPI.Integration/Data/Dtos/Register/InvalidRegisterDtoSource.cs
@@ -0,0 +1,25 @@
+﻿using IdentityAPI.Dtos;
+
+namespace IdentityAPI.Integration.Data.Dtos.Register;
+
+public static class InvalidRegisterDtoSource
+{
+    public static IEnumerable<RegisterDto> TestCases()
+    {
+        yield return new RegisterDto()
+        {
+            Username = "",
+            Password = "",
+        };
+        yield return new RegisterDto()
+        {
+            Username = "username",
+            Password = "This password doesnt contain a number",
+        };
+        yield return new RegisterDto()
+        {
+            Username = "username",
+            Password = "short1!",
+        };
+    }
+}
