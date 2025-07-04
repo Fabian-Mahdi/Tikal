3b8795094dac2ff23f14b8a70657de9afd99e51f
Fabian Mahdi <fabian@mahdi.at>
2025-07-04 21:23:44 +0200

    added unit tests for login controller

---
+++ IdentityAPI.Tests/Data/Dtos/LoginDtoSource.cs
@@ -0,0 +1,24 @@
+﻿using IdentityAPI.Dtos;
+
+namespace IdentityAPI.Tests.Data.Dtos;
+internal class LoginDtoSource
+{
+    public static IEnumerable<LoginDto> TestCases()
+    {
+        yield return new LoginDto()
+        {
+            Username = "username",
+            Password = "password",
+        };
+        yield return new LoginDto()
+        {
+            Username = "",
+            Password = "",
+        };
+        yield return new LoginDto()
+        {
+            Username = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+            Password = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+        };
+    }
+}
