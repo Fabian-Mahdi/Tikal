a56662678b4f4a9d695efe3134273260866a55e1
Fabian Mahdi <Fabian@Mahdi.at>
2025-07-19 15:49:04 +0200

    added RefreshTokens usecase

--- IdentityAPI/Authentication/Domain/DataAccess/TokenDataAccess.cs
+++ IdentityAPI/Authentication/Domain/DataAccess/TokenDataAccess.cs
@@ -5,4 +5,8 @@ namespace IdentityAPI.Authentication.Domain.DataAccess;
 public interface TokenDataAccess
 {
     (RefreshToken, AccessToken) GenerateTokenPair(User user);
+
+    Task<bool> ValidateToken(string token);
+
+    Task<T?> ExtractClaim<T>(string token, string claimName);
 }
