eb100cda979a5fad4c1f80166b9ead49b9180cce
Fabian Mahdi <Fabian@Mahdi.at>
2025-08-24 12:59:01 +0200

    added basic Button component

---
+++ Frontend/src/app/core/components/button/button.ts
@@ -0,0 +1,17 @@
+import { Component, ChangeDetectionStrategy, input } from "@angular/core";
+import { ButtonType } from "./button-type";
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-button",
+  imports: [],
+  templateUrl: "./button.html",
+  styleUrl: "./button.scss",
+})
+export class Button {
+  readonly type = input.required<ButtonType>();
+
+  get ButtonType(): typeof ButtonType {
+    return ButtonType;
+  }
+}
