a6b7f47e1e31a773a1a0a92a247e7b34c357d3c0
Fabian Mahdi <Fabian@Mahdi.at>
2025-08-24 17:20:48 +0200

    added play buttons in home screen

--- Frontend/src/app/core/home/home.ts
+++ Frontend/src/app/core/home/home.ts
@@ -1,11 +1,17 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { NgOptimizedImage } from "@angular/common";
+import { Button } from "../components/button/button";
+import { ButtonType } from "../components/button/button-type";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-home",
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, Button],
   templateUrl: "./home.html",
   styleUrl: "./home.scss",
 })
-export class Home {}
+export class Home {
+  get ButtonType(): typeof ButtonType {
+    return ButtonType;
+  }
+}
