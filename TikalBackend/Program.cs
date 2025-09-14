7f4e85638fdd4deb1f30ae5a8fc0ee190b3c0497
Fabian Mahdi <Fabian@Mahdi.at>
2025-09-14 12:55:22 +0200

    TikalBackend inital commit

---
+++ TikalBackend/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Shared.Extensions.LoggingBuilder;
+using Shared.Extensions.ServiceCollection;
+using Shared.Extensions.WebApp;
+using TikalBackend.Database;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.ConfigureOpenTelemetry("TikalBackend");
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDevDbContext<ApplicationDbContext>(builder, "TikalDatabase");
+    builder.Services.AddDevOpenTelemetry();
+}
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+    logging.CombineLogs = false;
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddHealthChecks();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.ApplyMigrations<ApplicationDbContext>();
+}
+
+app.UseHttpLogging();
+
+app.MapHealthChecks("/healthcheck");
+
+app.MapControllers();
+
+app.Run();
