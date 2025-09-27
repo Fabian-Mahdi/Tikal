bb27dedb9860c5c8682899247b6c8cd862ed6a05
Fabian Mahdi <Fabian@Mahdi.at>
2025-09-27 20:27:05 +0200

    added missing comment to CreateAccountCommandValidator

--- Tikal/Tikal.Application/Accounts/Commands/CreateAccount/CreateAccountCommandValidator.cs
+++ Tikal/Tikal.Application/Accounts/Commands/CreateAccount/CreateAccountCommandValidator.cs
@@ -2,6 +2,9 @@ using FluentValidation;
 
 namespace Tikal.Application.Accounts.Commands.CreateAccount;
 
+/// <summary>
+///     The validator for <see cref="CreateAccountCommand" />
+/// </summary>
 public class CreateAccountCommandValidator : AbstractValidator<CreateAccountCommand>
 {
     public CreateAccountCommandValidator()
