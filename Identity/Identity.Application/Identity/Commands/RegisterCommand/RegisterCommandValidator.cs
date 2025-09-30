fc53d189401a5d203ab24388b90c96e48b14868b
Fabian Mahdi <Fabian@Mahdi.at>
2025-09-30 21:07:08 +0200

    register validator now properly validates username and password

--- Identity/Identity.Application/Identity/Commands/RegisterCommand/RegisterCommandValidator.cs
+++ Identity/Identity.Application/Identity/Commands/RegisterCommand/RegisterCommandValidator.cs
@@ -9,7 +9,22 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
 {
     public RegisterCommandValidator()
     {
-        RuleFor(x => x.username).NotEmpty();
-        RuleFor(x => x.password).NotEmpty();
+        RuleFor(x => x.username)
+            .MinimumLength(4)
+            .WithMessage("Username must be at least 4 characters long.")
+            .MaximumLength(20)
+            .WithMessage("Username must be at most 20 characters long.");
+
+        RuleFor(x => x.password)
+            .MinimumLength(6)
+            .WithMessage("Password must be at least 6 characters long")
+            .Matches("[A-Z]")
+            .WithMessage("Password must contain at least one uppercase letter")
+            .Matches("[a-z]")
+            .WithMessage("Password must contain at least one lowercase letter")
+            .Matches("[0-9]")
+            .WithMessage("Password must contain at least one digit")
+            .Matches("[^a-zA-Z0-9]")
+            .WithMessage("Password must contain at least one special letter");
     }
 }
