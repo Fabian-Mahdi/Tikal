9dd078e99302422ff69b87a032be165915c4233f
Fabian Mahdi <Fabian@Mahdi.at>
2025-10-13 19:52:59 +0200

    added create account functionality

--- Frontend/src/app/features/authentication/components/create-account/create-account.ts
+++ Frontend/src/app/features/authentication/components/create-account/create-account.ts
@@ -10,6 +10,9 @@ import {
 } from "@angular/forms";
 import { Button } from "../../../../core/components/button/button";
 import { ButtonStyle } from "../../../../core/components/button/button-type";
+import { LoadingOverlayService } from "../../../../core/loading-overlay/loading-overlay-service";
+import { CreateAccountUseCase } from "../../usecases/create-account/create-account-usecase";
+import { ErrorOverlayService } from "../../../../core/error-overlay/error-overlay-service";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,6 +24,16 @@ import { ButtonStyle } from "../../../../core/components/button/button-type";
 })
 export class CreateAccount {
   private readonly router: Router = inject(Router);
+
+  private readonly createAccount: CreateAccountUseCase =
+    inject(CreateAccountUseCase);
+
+  private readonly loadingOverlay: LoadingOverlayService = inject(
+    LoadingOverlayService,
+  );
+
+  private readonly errorOverlay: ErrorOverlayService =
+    inject(ErrorOverlayService);
 
   private readonly formBuilder: NonNullableFormBuilder = inject(
     NonNullableFormBuilder,
@@ -35,6 +48,24 @@ export class CreateAccount {
       this.accountForm.markAllAsTouched();
       return;
     }
+
+    this.loadingOverlay.showLoadingOverlay();
+
+    const { name }: { name: string } = this.accountForm.value;
+
+    const accountCreationResult = await this.createAccount.call(name);
+
+    if (accountCreationResult.isErr()) {
+      this.loadingOverlay.hideLoadingOverlay();
+      this.router.navigate([{ outlets: { overlay: null } }]);
+      this.errorOverlay.showErrorOverlay(
+        "An account for your user already exists",
+      );
+      return;
+    }
+
+    this.loadingOverlay.hideLoadingOverlay();
+    this.router.navigate([{ outlets: { overlay: null } }]);
   }
 
   onCancelPressed(): void {
