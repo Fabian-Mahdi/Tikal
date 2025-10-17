982e977d16b55964ec6a9aa59811ade8d9ae05c8
Fabian Mahdi <Fabian@Mahdi.at>
2025-10-17 21:37:15 +0200

    removed angular animations

--- Frontend/src/app/core/menu/menu.ts
+++ Frontend/src/app/core/menu/menu.ts
@@ -1,11 +1,4 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
-import {
-  container1LeaveAnimation,
-  container2LeaveAnimation,
-  container3LeaveAnimation,
-  container4LeaveAnimation,
-  mainLeaveAnimation,
-} from "./animations/leave-animations";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,12 +6,5 @@ import {
   imports: [],
   templateUrl: "./menu.html",
   styleUrl: "./menu.scss",
-  animations: [
-    mainLeaveAnimation,
-    container1LeaveAnimation,
-    container2LeaveAnimation,
-    container3LeaveAnimation,
-    container4LeaveAnimation,
-  ],
 })
 export class Menu {}
