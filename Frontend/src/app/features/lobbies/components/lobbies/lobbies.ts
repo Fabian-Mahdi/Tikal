94d84642b2f78b514d6c0a1405916e558403fad2
Fabian Mahdi <Fabian@Mahdi.at>
2025-11-08 14:22:07 +0100

    removed commented out code

--- Frontend/src/app/features/lobbies/components/lobbies/lobbies.ts
+++ Frontend/src/app/features/lobbies/components/lobbies/lobbies.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectionStrategy, inject } from "@angular/core";
-import { Button } from "../../../../core/components/button/button";
 import { ButtonStyle } from "../../../../core/components/button/button-type";
 import { Dispatcher } from "@ngrx/signals/events";
 import { globalEvents } from "../../../../core/events/global-events";
@@ -7,7 +6,6 @@ import { globalEvents } from "../../../../core/events/global-events";
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-lobbies",
-  imports: [Button],
   templateUrl: "./lobbies.html",
   styleUrl: "./lobbies.scss",
 })
