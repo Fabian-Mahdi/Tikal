1ed5011e9e3a253cad6fed0b4a217f54990f5d81
Fabian Mahdi <Fabian@Mahdi.at>
2025-11-15 20:57:08 +0100

    auth interceptor tests

--- Frontend/src/app/core/interceptors/authentication/authentication-interceptor.spec.ts
+++ Frontend/src/app/core/interceptors/authentication/authentication-interceptor.spec.ts
@@ -1,17 +1,44 @@
 import { TestBed } from "@angular/core/testing";
-import { HttpInterceptorFn } from "@angular/common/http";
-
+import { TokenStatus, TokenStore } from "../../../features/authentication/stores/token/token-store";
+import { provideZonelessChangeDetection, signal } from "@angular/core";
+import { HttpClient, provideHttpClient, withInterceptors } from "@angular/common/http";
 import { authenticationInterceptor } from "./authentication-interceptor";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { firstValueFrom } from "rxjs";
 
 describe("authenticationInterceptor", () => {
-  const interceptor: HttpInterceptorFn = (req, next) =>
-    TestBed.runInInjectionContext(() => authenticationInterceptor(req, next));
+  const testUrl = "testing";
+
+  const tokenStore = {
+    token: signal("token"),
+    status: signal(TokenStatus.success),
+  };
+
+  let httpClient: HttpClient;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        {
+          provide: TokenStore,
+          useValue: tokenStore,
+        },
+        provideHttpClient(withInterceptors([authenticationInterceptor])),
+        provideHttpClientTesting(),
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it("should be created", () => {
-    expect(interceptor).toBeTruthy();
+  it("should add the stored token as bearer token in the Authorization header", () => {
+    firstValueFrom(httpClient.get(testUrl));
+
+    const req = httpTesting.expectOne(testUrl);
+
+    expect(req.request.headers.get("Authorization")).toEqual(`Bearer ${tokenStore.token()}`);
   });
 });
